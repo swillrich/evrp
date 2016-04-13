a8c956eb31df09296090944add77c51967d2343d
Sven Willrich <sven.willrich@googlemail.com>
2016-04-13 12:08:50 +0200

    move test data to test directory

--- core/src/main/java/de/fuberlin/winfo/project/algorithm/impl/sven/vns/KOptHeuristic.java
+++ core/src/main/java/de/fuberlin/winfo/project/algorithm/impl/sven/vns/KOptHeuristic.java
@@ -9,11 +9,7 @@ import java.util.TreeMap;
 
 import de.fuberlin.winfo.project.algorithm.impl.sven.vns.KOptHeuristic.Option;
 import de.fuberlin.winfo.project.model.network.Edge;
-import de.fuberlin.winfo.project.model.network.Node;
-import de.fuberlin.winfo.project.model.network.impl.NetworkFactoryImpl;
 import de.fuberlin.winfo.project.model.network.solution.Route;
-import de.fuberlin.winfo.project.model.network.solution.UsedEdge;
-import de.fuberlin.winfo.project.model.network.solution.impl.SolutionFactoryImpl;
 
 public class KOptHeuristic implements Iterator<Option> {
 	private int[] posArr;
@@ -21,7 +17,7 @@ public class KOptHeuristic implements Iterator<Option> {
 	private int k;
 	private Route route;
 
-	KOptHeuristic(int k, Route route) throws Exception {
+	public KOptHeuristic(int k, Route route) throws Exception {
 		int edges = route.getWay().size();
 		if (edges < 3 || k < 2) {
 			throw new Exception("At least both 3 edges and 2-opt needed");
@@ -202,61 +198,5 @@ public class KOptHeuristic implements Iterator<Option> {
 			}
 			return b.toString();
 		}
-	}
-
-	public static void main(String[] args) {
-		Route example = getExample(10);
-		try {
-			KOptHeuristic opt = new KOptHeuristic(3, example);
-			opt.printAll();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static Route getExample(int n) {
-		NetworkFactoryImpl f = new NetworkFactoryImpl();
-		SolutionFactoryImpl ff = new SolutionFactoryImpl();
-
-		Node[] nodes = new Node[n];
-		Route route = ff.createRoute();
-		for (int i = 0; i < nodes.length; i++) {
-			nodes[i] = f.createNode();
-			nodes[i].setId(i);
-		}
-
-		Edge[][] edges = new Edge[(int) Math.pow(nodes.length, 2)][(int) Math.pow(nodes.length, 2)];
-
-		for (int i = 0; i < nodes.length; i++) {
-			for (int j = 0; j < nodes.length; j++) {
-				Edge edge = f.createEdge();
-				edge.setStart(nodes[i]);
-				edge.setEnd(nodes[j]);
-				nodes[i].getEdgeOut().add(edge);
-				nodes[j].getEdgeIn().add(edge);
-				edge.setDistance((i + j) / 2);
-				edges[i][j] = edge;
-				// System.out.println("Create edge: " + edge.getStart().getId()
-				// + " -> " + edge.getEnd().getId());
-			}
-		}
-
-		for (int i = 0; i + 1 < nodes.length; i++) {
-			Edge edge = edges[nodes[i].getId()][nodes[i + 1].getId()];
-			UsedEdge usedEdge = ff.createUsedEdge();
-			usedEdge.setEdge(edge);
-			route.getWay().add(usedEdge);
-		}
-		UsedEdge usedEdge = ff.createUsedEdge();
-		Edge edge = edges[route.getWay().get(route.getWay().size() - 1).getEdge().getEnd().getId()][0];
-		usedEdge.setEdge(edge);
-		route.getWay().add(usedEdge);
-		System.out.println("Created route:");
-		for (UsedEdge e : route.getWay()) {
-			System.out.print(e.getEdge().getStart().getId() + " -> ");
-		}
-		System.out.print(route.getWay().get(route.getWay().size() - 1).getEdge().getEnd().getId());
-		System.out.println();
-		return route;
 	}
 }
