4d30d5200b02a3dcd78a2b6dd83cba70eeafcb14
Sven Willrich <sven.willrich@googlemail.com>
2016-04-20 14:47:52 +0200

    create custom table printer class for history

--- common/src/main/java/de/fuberlin/winfo/project/TablePrinter.java
+++ common/src/main/java/de/fuberlin/winfo/project/TablePrinter.java
@@ -1,54 +1,95 @@
 package de.fuberlin.winfo.project;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TablePrinter {
-	String[] titles = "#, Prev Costs, New Costs, diff".split(", ");
-	String format = "";
-	int border = 8;
-	String separator = "|  ";
-	int rows = 0;
-	char lineChar = '=';
+	private String[] titles;
+	private String format;
+	private String separator = "| ";
+	private int row = -1;
+	private char lineChar = '=';
+	private PrintStream printStream;
+	private int numberingIndent = 5;
+	private String[] columnFormat;
 
-	public void setTitles(String[] titles) {
+	public TablePrinter(PrintStream stream, String... titles) {
+		this.printStream = stream;
 		this.titles = titles;
+		format = "";
+		columnFormat = new String[titles.length];
+		for (int i = 0; i < columnFormat.length; i++) {
+			columnFormat[i] = "%-" + titles[i].length() + "s";
+		}
+		buildFormat();
 	}
 
-	private void init() {
-		format = "";
-		for (String title : titles) {
-			int length = title.length() + border;
-			format += "%-" + (length + separator.length()) + "s";
-		}
-		System.out.println();
+	private void buildFormat() {
+		format = separator + Arrays.stream(columnFormat).collect(Collectors.joining(separator)) + separator + "%n";
+	}
+
+	public void setParam(int columnId, boolean alignLeft, int width) {
+		columnFormat[columnId] = "%" + (alignLeft ? "" : "-") + width + "s";
+		buildFormat();
+	}
+
+	public void setParam(int columnId, int width) {
+		columnFormat[columnId] = "%-" + width + "s";
+		buildFormat();
+	}
+
+	public void init() {
+		int lineLength = Arrays.stream(columnFormat)
+				.map(s -> s.replace("%", "").replace("-", "").replace("s", "").trim()).map(s -> Integer.valueOf(s))
+				.mapToInt(s -> (int) s).sum();
 		print(titles);
-		int lineLength = Arrays.stream(titles).mapToInt(String::length).sum() + border * titles.length
-				+ separator.length() * titles.length - 1;
-		IntStream.range(0, lineLength).forEach(i -> System.out.print(lineChar));
-		System.out.println();
+		lineLength += titles.length * separator.length() - 1;
+		IntStream.range(0, numberingIndent).forEach(i -> out(" "));
+		out(separator.toCharArray()[0]);
+		IntStream.range(0, lineLength).forEach(i -> out(lineChar));
+		out(separator.toCharArray()[0]);
+		newLine();
 	}
 
 	public void print(Object... content) {
-		if (rows == 0) {
-			rows++;
+		String sRow = "";
+		if (row == -1) {
+			row++;
 			init();
+			print(content);
+			return;
+		} else if (row > 0) {
+			sRow = row++ + ".";
+		} else if (row == 0) {
+			row++;
 		}
-		Object[] c = new Object[titles.length];
+		out(sRow);
+		IntStream.range(0, numberingIndent - sRow.length()).forEach(i -> out(" "));
+		String string = String.format(format, content);
+		out(string);
+	}
 
-		int offSet = titles.length - content.length;
-
-		if (offSet > 0) {
-			c[0] = "" + rows++;
+	private void out(Object o) {
+		String s = o + "";
+		try {
+			printStream.write(s.getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+	}
 
-		for (int i = offSet; i < titles.length; i++) {
-			c[i] = "" + content[i - offSet];
-			if (i > 0) {
-				c[i] = separator + c[i];
-			}
-		}
-		String string = String.format(format, c);
-		System.out.println(string);
+	private void newLine() {
+		out("\n");
+	}
+
+	public static void main(String[] args) {
+		TablePrinter printer = new TablePrinter(System.out, "Hello World", "B", "C", "D", "E");
+		printer.setParam(1, false, 10);
+		printer.setParam(2, true, 10);
+		printer.setParam(3, false, 10);
+		IntStream.range(0, 10).forEach(i -> printer.print(i * 2 + 1, i * 4, i * 2, i * 4, 1));
 	}
 }
--- core/src/main/java/de/fuberlin/winfo/project/algorithm/impl/sven/vns/VNSMonitor.java
+++ core/src/main/java/de/fuberlin/winfo/project/algorithm/impl/sven/vns/VNSMonitor.java
@@ -1,16 +1,23 @@
 package de.fuberlin.winfo.project.algorithm.impl.sven.vns;
 
-import static de.fuberlin.winfo.project.FormatConv.*;
+import static de.fuberlin.winfo.project.FormatConv.withSeparator;
+
 import de.fuberlin.winfo.project.TablePrinter;
 import de.fuberlin.winfo.project.model.network.solution.Solution;
 
 public class VNSMonitor {
-	private TablePrinter tablePrinter = new TablePrinter();
+	String[] titles = new String[] { "NH", "Prev Cost", "New Cost", "Diff", "Reason" };
+	private TablePrinter tablePrinter;
 	private CostFunction f;
-
+	
 	public VNSMonitor(CostFunction function) {
 		this.f = function;
-		tablePrinter.setTitles("#, NH, Prev Costs, New Costs, Diff, Reason".split(", "));
+		tablePrinter = new TablePrinter(System.out, titles);
+		tablePrinter.setParam(0, 9);
+		tablePrinter.setParam(1, 15);
+		tablePrinter.setParam(2, 15);
+		tablePrinter.setParam(3, 13);
+		tablePrinter.setParam(4, 20);
 	}
 
 	public void vnsUpdate(NeighborhoodStructure nb, int k, Solution prev, Solution better) {
@@ -28,5 +35,5 @@ public class VNSMonitor {
 		}
 
 		tablePrinter.print(name, prevString, betterString, diff, "no yet implemented");
-	} 
+	}
 }
